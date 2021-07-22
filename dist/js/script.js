53ebc6e663db4d864247e084ab49bb5f1682053b
Maxim <xbultazar@gmail.com>
2021-07-22 23:18:47 +0300

    finish layout

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,23 @@
 var s = skrollr.init();
+
+// menu
+
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
+
+document.onclick = function (e) {
+    if (e.target.id !== 'menu' && e.target.id !== 'toggle') {
+        toggle.classList.remove('active');
+        // menu.classList.remove('active');
+        overlay.classList.remove('active');
+        body.classList.remove('active');
+    }
+}
+toggle.onclick = function () {
+    toggle.classList.toggle('active');
+    // menu.classList.toggle('active');
+    overlay.classList.toggle('active');
+    body.classList.toggle('active');
+}
