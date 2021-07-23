8a29bca76a3706c87efebb545a68d53af6b9e7a9
Maxim <xbultazar@gmail.com>
2021-07-23 21:21:24 +0300

    add eng text

--- src/js/script.js
+++ src/js/script.js
@@ -10,14 +10,12 @@ const body = document.querySelector('body');
 document.onclick = function (e) {
     if (e.target.id !== 'menu' && e.target.id !== 'toggle') {
         toggle.classList.remove('active');
-        // menu.classList.remove('active');
         overlay.classList.remove('active');
         body.classList.remove('active');
     }
 }
 toggle.onclick = function () {
     toggle.classList.toggle('active');
-    // menu.classList.toggle('active');
     overlay.classList.toggle('active');
     body.classList.toggle('active');
 }
