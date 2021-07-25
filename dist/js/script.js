dca38d78f657653d0b0dfbbb40a2c9c8793ac069
Maxim <xbultazar@gmail.com>
2021-07-25 18:48:32 +0300

    finish static

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,14 @@
 var s = skrollr.init();
 
-// menu
-
 const toggle = document.querySelector('.toggle');
 const menu = document.querySelector('.menu');
 const overlay = document.querySelector('.overlay');
 const body = document.querySelector('body');
+const checkLang = document.querySelector('.check');
+const ruText = document.querySelectorAll('[data-lang="ru"]');
+const engText = document.querySelectorAll('[data-lang="eng"]');
 
+// menu
 document.onclick = function (e) {
     if (e.target.id !== 'menu' && e.target.id !== 'toggle') {
         toggle.classList.remove('active');
@@ -19,3 +21,41 @@ toggle.onclick = function () {
     overlay.classList.toggle('active');
     body.classList.toggle('active');
 }
+
+// проверка localStorage
+if (localStorage.checkLang) {
+    checkLang.checked = true;
+}
+
+if (localStorage.getItem('checkLang')) {
+    ruText.forEach(item => {
+        item.style.display = 'none';
+    })
+    engText.forEach(item => {
+        item.style.display = 'block';
+    })
+}
+
+//toggle langueges
+
+checkLang.addEventListener('change', () => {
+    if (checkLang.checked) {
+        localStorage.setItem('checkLang', true);
+
+        ruText.forEach(item => {
+            item.style.display = 'none';
+        })
+        engText.forEach(item => {
+            item.style.display = 'block';
+        })
+    } else {
+        localStorage.removeItem('checkLang');
+
+        ruText.forEach(item => {
+            item.style.display = 'block';
+        })
+        engText.forEach(item => {
+            item.style.display = 'none';
+        })
+    }
+})
